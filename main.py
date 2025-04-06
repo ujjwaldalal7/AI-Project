b1e65ad613bb82b28bea25cab0509ec440caf992
ujjwaldalal7 <ujjwaldalal1611@gmail.com>
2025-04-06 20:20:34 +0530

    FPS tracked

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import mediapipe
 import cv2
 import numpy as np
-
+import time as time
 mp_drawing = mediapipe.solutions.drawing_utils
 mp_hands = mediapipe.solutions.hands
 mp_drawing_styles = mediapipe.solutions.drawing_styles
-
 
 def main():
     cap = cv2.VideoCapture(0)
@@ -15,6 +14,9 @@ def main():
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5,
     )
+
+    prev_time = 0  # <-- Initialize here
+
     while cap.isOpened():
         ret, image = cap.read()
         if not ret:
@@ -36,10 +38,19 @@ def main():
                     mp_drawing_styles.get_default_hand_connections_style(),
                 )
 
+      
+        curr_time = time.time()
+        fps = 1 / (curr_time - prev_time) if prev_time != 0 else 0
+        prev_time = curr_time
+
+        cv2.putText(image, f'FPS: {int(fps)}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
+                    1, (255, 0, 0), 2)
+
         cv2.imshow("Hand Tracking", image)
 
         if cv2.waitKey(5) & 0xFF == 27:
             break
+
     hands.close()
     cap.release()
     cv2.destroyAllWindows()
